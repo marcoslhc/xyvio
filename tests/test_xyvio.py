338b3bdf65f34397ea74749fe8fd967a2fce0791
Marcos Hernández <marcoslhc@gmail.com>
2015-09-22 18:30:57 -0400

    define tests

--- tests/test_xyvio.py
+++ tests/test_xyvio.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from werkzeug.test import Client
+from xyvio.xivyo import app
+from werkzeug.wrappers import BaseResponse
 """
 test_xyvio
 ----------------------------------
@@ -16,9 +18,30 @@ from xyvio import xyvio
 class TestXyvio(unittest.TestCase):
 
     def setUp(self):
+        self.client = Client(app, BaseResponse)
+
+    def test_base36_encode(self):
         pass
 
-    def test_something(self):
+    def test_render_template(self):
+        pass
+
+    def test_dispatch_request(self):
+        pass
+
+    def test_on_new_url(self):
+        pass
+
+    def test_insert_url(self):
+        pass
+
+    def test_on_follow_short_link(self):
+        pass
+
+    def test_on_short_link_details(self):
+        pass
+
+    def test_is_valid_url(self):
         pass
 
     def tearDown(self):
