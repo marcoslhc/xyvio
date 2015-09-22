e3966aad755b0d55d29f093cb861135c3ab7544f
Marcos Hernández <marcoslhc@gmail.com>
2015-09-22 18:47:37 -0400

    (maually) Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='xyvio',
-    version='0.1.0',
+    version='0.1.1',
     description="Basic Url Shortener",
     long_description=readme + '\n\n' + history,
     author="Marcos Hernández",
--- xyvio/__init__.py
+++ xyvio/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Marcos Hernández'
 __email__ = 'marcoslhc@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
